bb16e03c75d89708eed1a71d06beb589ff6bc30c
Gustavo Matos <37024361+guMatos@users.noreply.github.com>
2020-01-18 15:01:42 -0300

    create app.js

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config({ path: '.env' })
+
+const app = express()
+const PORT = process.env.PORT
+const MONGOURL = process.env.MONGOURL
+
+mongoose.connect(MONGOURL)
+mongoose.Promise = global.Promise
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.listen(PORT, () => console.log('App is running at local port ' + PORT))
