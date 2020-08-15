8f1cc6034331e3e9404e6b5f0ef5be34172a4745
Gustavo Matos <37024361+guMatos@users.noreply.github.com>
2020-08-14 21:04:33 -0300

    Update token authentication

--- src/middleware/jwtExtensions.js
+++ src/middleware/jwtExtensions.js
@@ -3,8 +3,9 @@ require('dotenv').config({ path: '.env' })
 
 class jwtExtensions {
 	async verifyJWT(req, res, next) {
-		var token = req.headers['x-access-token']
+		var token = req.headers['authorization']
 		if (!token) return res.status(401).send()
+		token = token.substring(7)
 
 		jwt.verify(token, process.env.SECRET, (err) => {
 			if (err) return res.status(401).send({ message: 'failed to authenticate token' })
